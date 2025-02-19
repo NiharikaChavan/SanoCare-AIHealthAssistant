a7fe727cbf9dd31be876a70a3bf86d860d8af51e
NiharikaChavan <niharikachavan1997@gmail.com>
2025-02-18 20:36:54 -0700

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Sanocare - AI Health Assistant',
+    version= '0.0.0',
+    author= 'Niharika Chavan',
+    author_email= 'nchavant@arizona.edu',
+    packages= find_packages(),
+    install_requires = []
+
+)
